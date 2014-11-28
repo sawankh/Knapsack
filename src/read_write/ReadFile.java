c54b1618465dd207491b6fa39b6ea6ebf4c60c9f
Sawan J. Kapai Harpalani <alu0100694765@ull.edu.es>
2014-11-28 19:16:20 +0000

    Added getTime and getNumObjects methods

--- src/read_write/ReadFile.java
+++ src/read_write/ReadFile.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.StringTokenizer;
 
 // TODO: Auto-generated Javadoc
 /**
@@ -86,14 +88,15 @@ public class ReadFile {
 	 * @throws IOException
 	 *             Signals that an I/O exception has occurred.
 	 */
-	public static void getAllFiles(String directory, ArrayList<String> file_name) throws IOException {
+	public static void getAllFiles(String directory, ArrayList<String> file_name)
+			throws IOException {
 		File f = new File(directory);
 		File[] listfiles = f.listFiles();
 		for (int i = 0; i < listfiles.length; i++) {
 			if (listfiles[i].isDirectory()) {
 				File[] internalFile = listfiles[i].listFiles();
 				for (int j = 0; j < internalFile.length; j++) {
-					//System.out.println(internalFile[j].getPath());
+					// System.out.println(internalFile[j].getPath());
 					file_name.add(internalFile[j].getPath());
 					if (internalFile[j].isDirectory()) {
 						String name = internalFile[j].getAbsolutePath();
@@ -101,11 +104,68 @@ public class ReadFile {
 					}
 				}
 			} else {
-				 //System.out.println(listfiles[i]);
-				 file_name.add(listfiles[i].getPath());
+				// System.out.println(listfiles[i]);
+				file_name.add(listfiles[i].getPath());
 			}
 
 		}
 	}
 
+	/**
+	 * Extract time.
+	 * Extracts the time of execution of each result.
+	 * 
+	 * @param result_array the result_array
+	 */
+	public void extractTime(ArrayList<Integer> result_array) {
+		final int TIME = 1;
+		
+		for (Iterator<String> iterator = content.iterator(); iterator.hasNext();) {
+			String line = (String) iterator.next();
+			StringTokenizer tokenizer = new StringTokenizer(line, "\t");
+
+			int number_token = 0;
+			
+			while (tokenizer.hasMoreTokens()) {
+				String data = tokenizer.nextToken();
+				
+				if (number_token == TIME) {
+					result_array.add(Integer.parseInt(data));
+				}
+				
+				number_token++;
+			}
+		}	
+	}
+	
+	/**
+	 * Extract num object.
+	 * Extracts the number of objects that had the problem
+	 * from each result
+	 *
+	 * @param result_array the result_array
+	 */
+	public void extractNumObject(ArrayList<Integer> result_array) {
+		final int NUM_OBJECTS = 2;
+		
+		for (Iterator<String> iterator = content.iterator(); iterator.hasNext();) {
+			String line = (String) iterator.next();
+			StringTokenizer tokenizer = new StringTokenizer(line, "\t");
+
+			int number_token = 0;
+			
+			while (tokenizer.hasMoreTokens()) {
+				String data = tokenizer.nextToken();
+				
+				if (number_token == NUM_OBJECTS) {
+					if (!result_array.contains(Integer.parseInt(data))) {
+						result_array.add(Integer.parseInt(data));
+					}
+				}
+				
+				number_token++;
+			}
+		}	
+	}
+	
 }
