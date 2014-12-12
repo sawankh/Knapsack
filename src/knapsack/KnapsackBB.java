f4d98986d9c9c4ba40603ddbd61466c29c29985b
Sawan J. Kapai Harpalani <alu0100694765@ull.edu.es>
2014-12-12 16:53:48 +0000

    Added Branch N Bound Class

---
+++ src/knapsack/KnapsackBB.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package knapsack;
+
+import interface_knapsack.IKnapsack;
+
+/**
+ * @author Sawan
+ *
+ */
+public class KnapsackBB implements IKnapsack {
+
+	@Override
+	public void solveKnapsack() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public float getKnapsackSolution() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
