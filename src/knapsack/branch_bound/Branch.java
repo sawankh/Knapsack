c1eb8565f3167878c63946d6b34e1e958a3197ec
Sawan J. Kapai Harpalani <alu0100694765@ull.edu.es>
2014-12-13 15:32:07 +0000

    Branch and Bound implemented it seems to be working quite good

--- src/knapsack/branch_bound/Branch.java
+++ src/knapsack/branch_bound/Branch.java
@@ -4,9 +4,138 @@
 package knapsack.branch_bound;
 
 /**
- * @author Sawan
+ * The Class Branch.
  *
+ * @author Sawan
  */
-public class Branch {
+public class Branch implements Cloneable{
+	
+	/** The data. */
+	private Integer[] data;
+	
+	/** The solution. */
+	private double solution;
+	
+	private boolean last_node;
+	
+	private int weight;
+	
+	/**
+	 * Instantiates a new branch.
+	 *
+	 * @param size_data the size_data
+	 */
+	public Branch(int size_data) {
+		data =  new Integer[size_data];
+		solution = 0;
+		last_node = false;
+		weight = 0;
+	}
+	
+	public Branch(Branch branch) {
+		
+	}
+	
+	/**
+	 * Initialize data.
+	 */
+	public void initializeData() {
+		for (int i = 0; i < data.length; i++) {
+			data[i] = null;
+		}
+	}
+	
+	/**
+	 * Update data.
+	 *
+	 * @param position the position
+	 * @param value the value
+	 */
+	public void updateData(int position, Integer value) {
+		data[position] = new Integer(value);
+	}
 
+	public void printData() {
+		for (int i = 0; i < data.length; i++) {
+			System.out.println(data[i]);
+		}
+	}
+	
+	/**
+	 * @return the data
+	 */
+	public Integer[] getData() {
+		return data;
+	}
+
+	/**
+	 * @return the solution
+	 */
+	public double getSolution() {
+		return solution;
+	}
+
+	/**
+	 * @param solution the solution to set
+	 */
+	public void setSolution(double solution) {
+		this.solution = solution;
+	}
+
+	/**
+	 * @return the last_node
+	 */
+	public boolean isLast_node() {
+		return last_node;
+	}
+
+	/**
+	 * @param last_node the last_node to set
+	 */
+	public void setLast_node(boolean last_node) {
+		this.last_node = last_node;
+	}
+	
+	
+	
+	/**
+	 * @param data the data to set
+	 */
+	public void setData(Integer[] data) {
+		this.data = data;
+	}
+
+	public int getLevel() {
+		int level = 0;
+		
+		for (int i = 0; i < data.length; i++) {
+			if (data[i] == null) {
+				level = i;
+				break;
+			}
+		}
+		return level;
+	}
+	
+	/**
+	 * @return the weight
+	 */
+	public int getWeight() {
+		return weight;
+	}
+
+	/**
+	 * @param weight the weight to set
+	 */
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public static Branch copyBranch(Branch branch) {
+		Branch branch_result = new Branch(branch.getData().length);
+		System.arraycopy(branch.getData(), 0, branch_result.getData(), 0, branch.getData().length);
+		branch_result.setLast_node(branch.isLast_node());
+		branch_result.setSolution(branch.getSolution());
+		return branch_result;
+	}
 }
--- src/knapsack/branch_bound/KnapsackBB.java
+++ src/knapsack/branch_bound/KnapsackBB.java
@@ -3,10 +3,13 @@
  */
 package knapsack.branch_bound;
 
-import java.util.ArrayList;
-
 import interface_knapsack.IKnapsack;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
+// TODO: Auto-generated Javadoc
 /**
  * The Class KnapsackBB.
  * 
@@ -29,7 +32,14 @@ public class KnapsackBB implements IKnapsack {
 	/** The branch_bound_tree. */
 	private ArrayList<Branch> branch_bound_tree;
 
-	private int knapsack_solution;
+	private ArrayList<Double> branches_solution;
+
+	/** The knapsack_solution. */
+	private double knapsack_solution;
+
+	private final static Integer PICK_OBJECT = 1;
+
+	private final static Integer DONT_PICK_OBJECT = 0;
 
 	/**
 	 * Instantiates a new knapsack bb.
@@ -49,6 +59,7 @@ public class KnapsackBB implements IKnapsack {
 		object_weights = weights;
 		object_values = values;
 		branch_bound_tree = new ArrayList<Branch>();
+		branches_solution = new ArrayList<Double>();
 		knapsack_solution = 0;
 	}
 
@@ -59,7 +70,7 @@ public class KnapsackBB implements IKnapsack {
 	 * 
 	 * @return the int
 	 */
-	public int initialSolution() {
+	public double initialSolution() {
 		int solution_weight = 0;
 		int solution_value = 0;
 		for (int i = 0; i < object_values.length; i++) {
@@ -76,19 +87,144 @@ public class KnapsackBB implements IKnapsack {
 
 	/**
 	 * Intial branch.
-	 *
+	 * 
 	 * Creates an initial point to develop the tree
-	 *
+	 * 
 	 * @return the integer[]
 	 */
-	public Integer[] intialBranch() {
-		Integer[] branch =  new Integer[number_objects];
+	public void intialBranch() {
+		// Initial branches one intiates to 0 and the other to 1
+		Branch branch_0 = new Branch(number_objects);
+		Branch branch_1 = new Branch(number_objects);
+
+		// Set branch_0 to 0 and branch_1 to 1
+		branch_0.updateData(0, DONT_PICK_OBJECT);
+		branch_1.updateData(0, PICK_OBJECT);
+
+		// Get solution of each branch
+		calculateSolution(branch_0);
+		calculateSolution(branch_1);
+
 		
-		for (int i = 0; i < branch.length; i++) {
-			branch[i] = null;
+		// If the sol is valid, means, greater than the actual solution add
+		// branch
+		addBranch(branch_0);
+		addBranch(branch_1);
+
+		// Check if they are last nodes
+		updateSolution(branch_0, branch_1);
+		
+		cutBranches();
+	}
+
+	public void updateSolution(Branch branch_0, Branch branch_1) {
+		if ((branch_0.getWeight() <= knapsack_capacity) && (branch_1.getWeight() <= knapsack_capacity)) {
+			if (branch_0.isLast_node() && branch_1.isLast_node()) {
+				knapsack_solution = Math.max(branch_0.getSolution(), branch_1.getSolution());
+				if (branch_bound_tree.contains(branch_0)) {
+					branch_bound_tree.remove(branch_0);
+				}
+				
+				if (branch_bound_tree.contains(branch_1)) {
+					branch_bound_tree.remove(branch_1);
+				}
+			}
+		} else if ((branch_0.getWeight() <= knapsack_capacity) && (branch_0.isLast_node())) {
+			knapsack_solution = branch_0.getSolution();
+			if (branch_bound_tree.contains(branch_0)) {
+				branch_bound_tree.remove(branch_0);
+			}
+			
+			if (branch_bound_tree.contains(branch_1)) {
+				branch_bound_tree.remove(branch_1);
+			}
+		} else if ((branch_1.getWeight() <= knapsack_capacity) && (branch_1.isLast_node())) {
+			knapsack_solution = branch_1.getSolution();
+			if (branch_bound_tree.contains(branch_0)) {
+				branch_bound_tree.remove(branch_0);
+			}
+			
+			if (branch_bound_tree.contains(branch_1)) {
+				branch_bound_tree.remove(branch_1);
+			}
 		}
+	}
+	
+	public void addBranch(Branch branch) {
+		if (branch.getWeight() <= knapsack_capacity) {
+			branch_bound_tree.add(branch);
+			branches_solution.add(branch.getSolution());
+		}
+	}
+
+	/**
+	 * Calculate solution of a set.
+	 * 
+	 * @param branch
+	 *            the data
+	 * 
+	 */
+	public void calculateSolution(Branch branch) {
+		double solution = 0;
+
+		int sum_values = 0;
+		int sum_weight = 0;
+		int iterator_data = 0;
+
+		while (((iterator_data < branch.getData().length)) && (branch.getData()[iterator_data] != null)) {
+			sum_values += (branch.getData()[iterator_data] * object_values[iterator_data]);
+			sum_weight += (branch.getData()[iterator_data] * object_weights[iterator_data]);
+			iterator_data++;
+		}
+			if (branch.getData().length == iterator_data) {
+			solution = sum_values;
+			branch.setLast_node(true);
+		} else {
+			double next_average = ((double) object_values[iterator_data] / (double) object_weights[iterator_data]);
+			solution = sum_values
+					+ ((knapsack_capacity - sum_weight) * next_average);
+		}
+			
+		branch.setWeight(sum_weight);
+		branch.setSolution(solution);
+	}
+
+	public void createLevel(int position, Branch branch_father) {
+		// Branches one intiates to 0 and the other to 1
+		Branch branch_0 = Branch.copyBranch(branch_father);
+		Branch branch_1 = Branch.copyBranch(branch_father);
+
+		// Set branch_0 to 0 and branch_1 to 1
+		// NO HACE 0 a 0
+		branch_1.updateData(position, PICK_OBJECT);
+		branch_0.updateData(position, DONT_PICK_OBJECT);
 		
-		return branch;
+		
+		// Get solution of each branch
+		calculateSolution(branch_0);
+		calculateSolution(branch_1);
+
+		// If the sol is valid, means, greater than the actual solution add
+		// branch
+		addBranch(branch_0);
+		addBranch(branch_1);
+		
+		// Check if they are last nodes
+		updateSolution(branch_0, branch_1);
+		
+		
+	}
+
+	public void cutBranches() {
+		Iterator<Branch> iterator = branch_bound_tree.iterator();
+		while (iterator.hasNext()) {
+			Branch branch = (Branch) iterator.next();
+			if (branch.getSolution() <= knapsack_solution) {
+				iterator.remove();
+			}
+		}
+			
+		
 	}
 	
 	/*
@@ -98,12 +234,57 @@ public class KnapsackBB implements IKnapsack {
 	 */
 	@Override
 	public void solveKnapsack() {
-		// Get an initial solution 
+		// Get an initial solution
 		knapsack_solution = initialSolution();
-		
-		
+
+		// Add initial branches
+		intialBranch();
+
+		while (!branch_bound_tree.isEmpty()) {
+			
+			// Get Branch with highest solution value
+			int index_max_solution = getBranch(Collections
+					.max(branches_solution));
+			Branch branch_max_solution = Branch.copyBranch(branch_bound_tree
+					.get(index_max_solution));
+			
+			// Create next level
+			createLevel(branch_max_solution.getLevel(), branch_max_solution);
+			
+			cutBranches();
+			
+			// Remove father branch
+			if (!branch_bound_tree.isEmpty()) {
+				branch_bound_tree.remove(index_max_solution);
+			}
+			
+			branches_solution.remove(branches_solution.indexOf(branch_max_solution.getSolution()));
+			
+		}
+
 	}
 
+	public int getBranch(double solution) {
+		int index = -1;
+		for (Iterator<Branch> iterator = branch_bound_tree.iterator(); iterator.hasNext();) {
+			Branch branch = (Branch) iterator.next();
+			if (branch.getSolution() == solution) {
+				index = branch_bound_tree.indexOf(branch);
+				break;
+			}
+		}
+		
+		return index;
+	}
+	
+	public void printSolutions() {
+		for (Iterator<Double> iterator = branches_solution.iterator(); iterator.hasNext();) {
+			Double type = (Double) iterator.next();
+			System.out.println(type);
+		}
+		System.out.println("----------------------------------");
+	}
+	
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -111,7 +292,7 @@ public class KnapsackBB implements IKnapsack {
 	 */
 	@Override
 	public float getKnapsackSolution() {
-		return 0;
+		return (float) knapsack_solution;
 	}
 
 }
